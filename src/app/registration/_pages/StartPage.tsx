ebd3f8e6124f3a446d006ac52a9cd9f628e3e12a
sonjali4 <sonjali2006@gmail.com>
2026-07-30 01:46:58 +1200

    feat(registration): add styling to show upi/student id input fields conditionally

--- src/app/registration/_pages/StartPage.tsx
+++ src/app/registration/_pages/StartPage.tsx
@@ -10,45 +10,62 @@ export function StartPage({ fields }: { fields: Partial<RegistrationDraft> }) {
 
   return (
     <>
+      <style>
+        {`
+          .upi-id-input {
+            display: none;
+          }
+
+          input[type="radio"]:checked ~ .upi-id-input {
+            display: block;
+          }
+        `}
+      </style>
+
       <fieldset>
-        <legend>
-          Have you registered with us previously and meet the following
-          conditions?*
-        </legend>
-
-        <ul>
-          <li>You are a current student at the University of Auckland</li>
-          <li>
-            You previously gave us your UPI or Student ID when registering your
-            interest in 2025
-          </li>
-          <li>
-            You have not changed your programme of study since your last
-            application
-          </li>
-        </ul>
-
-        <p>
-          <i>
-            {"If you are signing up for the first time, you should select 'no'"}
-          </i>
-        </p>
+        <legend>Are you a UoA Student?*</legend>
 
         {state?.error?.includes("registered") && (
           <p className="text-red-600 text-sm italic">{state.error}</p>
         )}
 
         <div>
-          <label>
+          <input
+            type="radio"
+            name="isCurrentUoaStudent"
+            value="yes"
+            defaultChecked={field?.isCurrentUoaStudent === "yes"}
+            required
+          />
+          <label>Yes</label>
+
+          <div className="upi-id-input">
+            <label htmlFor="upi">What is your username/UPI?*</label>
+            <p>i.e. jbon007</p>
             <input
-              type="radio"
-              name="isCurrentUoaStudent"
-              value="yes"
-              defaultChecked={field?.isCurrentUoaStudent === "yes"}
+              name="upi"
+              id="upi"
+              type="text"
+              placeholder="Your answer"
+              defaultValue={field?.upi ?? ""}
+              pattern="[a-z]{3,4}[0-9]{3}"
               required
             />
-            Yes
-          </label>
+          </div>
+
+          <div className="upi-id-input">
+            <label htmlFor="studentId">And your student ID?*</label>
+            <p>i.e. 825179213</p>
+            <input
+              name="studentId"
+              id="studentId"
+              type="text"
+              placeholder="Your answer"
+              defaultValue={field?.studentId ?? ""}
+              pattern="[0-9]{9,10}"
+              required
+            />
+          </div>
 
           <label>
             <input
@@ -62,34 +79,6 @@ export function StartPage({ fields }: { fields: Partial<RegistrationDraft> }) {
           </label>
         </div>
       </fieldset>
-
-      <div>
-        <label htmlFor="upi">What is your username/UPI?*</label>
-        <p>i.e. jbon007</p>
-        <input
-          name="upi"
-          id="upi"
-          type="text"
-          placeholder="Your answer"
-          defaultValue={field?.upi ?? ""}
-          pattern="[a-z]{3,4}[0-9]{3}"
-          required
-        />
-      </div>
-
-      <div>
-        <label htmlFor="studentId">And your student ID?*</label>
-        <p>i.e. 825179213</p>
-        <input
-          name="studentId"
-          id="studentId"
-          type="text"
-          placeholder="Your answer"
-          defaultValue={field?.studentId ?? ""}
-          pattern="[0-9]{9,10}"
-          required
-        />
-      </div>
     </>
   );
 }
