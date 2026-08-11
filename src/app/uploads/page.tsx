588a01205ccd2d0fc02af84bb525b03baaef99a7
James Mullane <jamesmullane05@gmail.com>
2026-08-11 23:58:44 +1200

    feat(imagestore): created a working image storage

---
+++ src/app/uploads/page.tsx
@@ -0,0 +1,15 @@
+import uploadBlogImage from "@/features/blog/uploadBlogImage";
+
+export default function TestUploadPage() {
+  async function handleUpload(formData: FormData) {
+    const result = await uploadBlogImage(formData);
+    console.log(result);
+  }
+
+  return (
+    <form action={handleUpload}>
+      <input type="file" name="file" accept="image/*" />
+      <button type="submit">Upload</button>
+    </form>
+  );
+}
